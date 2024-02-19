c87a64cf23a03f21e7754485ea5a34a4d6bc9ed4
Ivan Dolgun <vanadolgun@gmail.com>
2024-02-20 00:28:27 +0700

    login
    added simple login logic

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './common.css';
+import { login } from "./Connector.js";
 
 class LoginPage extends React.Component {
-    handleLoginClick = () => {
-      // Перенаправление на главную страницу
-      window.location.href = '/main';
+    handleLoginClick = async () => {
+      let email = document.getElementById('field_email').value;
+      let password = document.getElementById('field_password').value;
+      let res = await login(email, password);
+      if (res != null) {
+        localStorage.setItem('keyGuardUserToken', res.access_token);
+        window.location.href = '/main';
+      }
     };
   
     render() {
@@ -17,16 +23,12 @@ class LoginPage extends React.Component {
                         <div class="raw-object w100-obj padding-h-normal text-huge text-dark text-width-bold margin-v-normal">Вход в аккаунт</div>
                     </div>
                     <div class="raw-object w100-obj margin-v-big content-left">
-                        <div class="raw-object w100-obj padding-h-normal text-normal text-default text-width-bold margin-v-normal">ФИО</div>
-                        <input type="text" class="raw-object raw-box w100-obj padding-h-normal padding-v-small text-normal text-light text-width-normal" placeholder="Фамилия Имя Отчество"/>
-                    </div>
-                    <div class="raw-object w100-obj margin-v-big content-left">
                         <div class="raw-object w100-obj padding-h-normal text-normal text-default text-width-bold margin-v-normal">E-mail</div>
-                        <input type="text" class="raw-object raw-box w100-obj padding-h-normal padding-v-small text-normal text-light text-width-normal" placeholder="email@example.com"/>
+                        <input type="text" id="field_email" class="raw-object raw-box w100-obj padding-h-normal padding-v-small text-normal text-light text-width-normal" placeholder="email@example.com"/>
                     </div>
                     <div class="raw-object w100-obj margin-v-big content-left">
                         <div class="raw-object w100-obj padding-h-normal text-normal text-default text-width-bold margin-v-normal">Пароль</div>
-                        <input type="password" class="raw-object raw-box w100-obj padding-h-normal padding-v-small text-normal text-light text-width-normal" placeholder="Напишите пароль"/>
+                        <input type="password" id="field_password" class="raw-object raw-box w100-obj padding-h-normal padding-v-small text-normal text-light text-width-normal" placeholder="Напишите пароль"/>
                     </div>
                     <div class="raw-object w100-obj margin-v-huge content-center">
                         <button onClick={this.handleLoginClick} class="raw-object w100-obj padding-v-normal raw-box button-default text-normal text-width-bold">
