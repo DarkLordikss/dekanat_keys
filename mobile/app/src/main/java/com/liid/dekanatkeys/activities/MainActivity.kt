c829229874ebde106c597ff0d3b2a337fb782718
E1 <ivanboitsov386@gmail.com>
2024-02-27 21:54:50 +0700

    Add navigation bar (only design)

--- mobile/app/src/main/java/com/liid/dekanatkeys/activities/MainActivity.kt
+++ mobile/app/src/main/java/com/liid/dekanatkeys/activities/MainActivity.kt
@@ -1,38 +1,35 @@
 package com.liid.dekanatkeys.activities
 
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.liid.dekanatkeys.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        getUserProfile();
-    }
+        val navView: BottomNavigationView = binding.navView
 
-//    private fun getUserProfile() {
-//        val preferences = getSharedPreferences(getString(R.string.SP_name), MODE_PRIVATE)
-//        val token = preferences.getString(getString(R.string.jwtTokenName), null)
-//        Log(if (token != null) "get$ token" else "token not found")
-//        val call: Call<UserProfile> = OKOApiSingleton.api.fetchUserProfile("bearer $token")
-//
-//        call.enqueue(OKOCallback<UserProfile>(
-//            successCallback = {response ->
-//                val userProfile = response.body()
-//                if (userProfile != null)
-//                {
-//                    binding.name.text = userProfile.name
-//                    binding.email.text = userProfile.email
-//                    binding.role.text = userProfile.role
-//                }
-//            },
-//            errorCallback = {response ->
-//                Log("error code:" + response.code().toString())
-//            }
-//        ))
-//    }
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
 }
