e77967fc7eeae7455048fc18ab807d7bb4627300
Liid <51267703+L11D@users.noreply.github.com>
2024-02-27 21:57:43 +0700

    Login and Register activities complete
    also add Success Registration activity

--- mobile/app/src/main/java/com/liid/dekanatkeys/activities/RegistrationActivity.kt
+++ mobile/app/src/main/java/com/liid/dekanatkeys/activities/RegistrationActivity.kt
@@ -3,12 +3,18 @@ package com.liid.dekanatkeys.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.BoringLayout
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import com.liid.dekanatkeys.R
 import com.liid.dekanatkeys.databinding.ActivityRegistrationBinding
 import com.liid.dekanatkeys.helpers.Log
+import com.liid.dekanatkeys.helpers.OKOApiSingleton
+import com.liid.dekanatkeys.helpers.OKOCallback
+import com.liid.dekanatkeys.models.user.RegistrationRequest
+import com.liid.dekanatkeys.models.user.RegistrationResponse
 
 class RegistrationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegistrationBinding
@@ -19,10 +25,30 @@ class RegistrationActivity : AppCompatActivity() {
     private lateinit var name: String
     private var nameError = true
 
+    private lateinit var emailErrorTextView : TextView
+    private lateinit var passwordErrorTextView : TextView
+    private lateinit var nameErrorTextView : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        emailErrorTextView = TextView(this).apply {
+            setTextColor(getColor(R.color.red))
+            text = getString(R.string.email_error_message)
+            textSize = 15F
+        }
+        passwordErrorTextView = TextView(this).apply {
+            setTextColor(getColor(R.color.red))
+            text = getString(R.string.password_error_message)
+            textSize = 15F
+        }
+        nameErrorTextView = TextView(this).apply {
+            setTextColor(getColor(R.color.red))
+            text = getString(R.string.name_error_message)
+            textSize = 15F
+        }
 
         binding.email.addTextChangedListener{ text ->
             testEmail(text.toString())
@@ -42,6 +68,9 @@ class RegistrationActivity : AppCompatActivity() {
             checkRegistrationButtonStatus()
         }
 
+        binding.registrationButton.setOnClickListener{
+            register(email, password, name)
+        }
 
         binding.loginButton.setOnClickListener{
             val intent = Intent(this@RegistrationActivity, LoginActivity::class.java)
@@ -49,58 +78,79 @@ class RegistrationActivity : AppCompatActivity() {
         }
     }
 
+    private fun register(email:String, password:String, name: String){
+        val registrationRequest = RegistrationRequest(email, password, name)
+
+        OKOApiSingleton.api.registrationUser(registrationRequest).enqueue(OKOCallback<RegistrationResponse>(
+            successCallback = {response ->
+                val intent = Intent(this@RegistrationActivity, SuccessRegistrationActivity::class.java)
+                startActivity(intent)
+            },
+            errorCallback = {response ->
+                Log("error code:" + response.code().toString())
+                if(response.code() == 400){
+                    Toast.makeText(this, "Пользователь уже существует", Toast.LENGTH_LONG).show()
+                }
+            }
+        ))
+    }
+
     private fun checkRegistrationButtonStatus(){
-        Log((!emailError && !passwordError && !nameError).toString())
         binding.registrationButton.isEnabled = !emailError && !passwordError && !nameError
+    }
+
+    private fun addChild(layout: LinearLayout, view: View){
+        if (layout.indexOfChild(view) == -1) layout.addView(view)
+    }
+    private fun removeChild(layout: LinearLayout, view: View){
+        if (layout.indexOfChild(view) != -1) layout.removeView(view)
     }
 
     private fun testEmail(email:String) {
         val emailRegex = "^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$".toRegex()
         setEmailError(!email.matches(emailRegex))
     }
-
     private fun setEmailError(error:Boolean) {
         if (error) {
             emailError = true
-            binding.emailErrorView.visibility = View.VISIBLE
             binding.email.setErrorColor()
+            addChild(binding.emailLayout, emailErrorTextView)
         } else {
             emailError = false
-            binding.emailErrorView.visibility = View.INVISIBLE
             binding.email.setColor()
+            removeChild(binding.emailLayout, emailErrorTextView)
         }
     }
 
     private fun testPassword(password:String) {
-        val passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*?&]{6,}$".toRegex()
-        setPasswordError(!password.matches(passwordRegex))
+//        val passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*?&]{6,}$".toRegex()
+//        setPasswordError(!password.matches(passwordRegex))
+        setPasswordError(password.length < 6)
     }
-
     private fun setPasswordError(error:Boolean) {
         if (error) {
             passwordError = true
-            binding.passwordErrorView.visibility = View.VISIBLE
             binding.password.setErrorColor()
+            addChild(binding.passwordLayout, passwordErrorTextView)
         } else {
             passwordError = false
-            binding.passwordErrorView.visibility = View.INVISIBLE
             binding.password.setColor()
+            removeChild(binding.passwordLayout, passwordErrorTextView)
         }
     }
 
     private fun testName(name:String) {
         setNameError(name.length !in 3..30)
     }
-
     private fun setNameError(error:Boolean) {
         if (error) {
             nameError = true
-            binding.nameErrorView.visibility = View.VISIBLE
             binding.name.setErrorColor()
+            addChild(binding.nameLayout, nameErrorTextView)
         } else {
             nameError = false
-            binding.nameErrorView.visibility = View.INVISIBLE
             binding.name.setColor()
+            removeChild(binding.nameLayout, nameErrorTextView)
         }
     }
 }
