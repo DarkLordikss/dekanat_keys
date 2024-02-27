afd5d03bf3a876f33baf9bb64f378d473165443c
Ivan Evchatov <minecraftmine161@gmail.com>
2024-02-27 14:49:09 +0700

    feat(building): Added getting building classrooms

--- backend/routers/building_router.py
+++ backend/routers/building_router.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from sqlalchemy.orm import Session
 
+from models.dto.building_classrooms_dto import BuildingClassroomsDTO
 from models.dto.building_dto import BuildingDTO
 from models.dto.error_dto import ErrorDTO
 from services.building_service import BuildingService
@@ -16,7 +17,7 @@ building_router = APIRouter(prefix="/building")
 
 
 @building_router.get(
-    "/",
+    "/get-all-buildings",
     response_model=BuildingDTO,
     responses={
         200: {
@@ -39,4 +40,41 @@ async def get_buildings(db: Session = Depends(get_db),
         return BuildingDTO(buildings=buildings)
     except Exception as e:
         logger.error(f"(Get all buildings) Error: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+
+@building_router.get(
+    "/get-classrooms-from-building",
+    response_model=BuildingClassroomsDTO,
+    responses={
+        200: {
+            "model": BuildingClassroomsDTO
+
+        },
+        404: {
+            "model": ErrorDTO
+        },
+        500: {
+            "model": ErrorDTO
+        }
+    }
+)
+async def get_building_classrooms(building: int,
+                                  db: Session = Depends(get_db),
+                                  building_service: BuildingService = Depends(BuildingService)
+                                  ):
+    try:
+        if building not in await building_service.get_all_buildings(db):
+            logger.warning(f"(Get building classrooms) Building not found: {building}")
+            raise HTTPException(status_code=404, detail="Building not found")
+
+        classrooms = await building_service.get_all_classrooms_from_building(db, building)
+
+        logger.info(f"(Get building classrooms) Successful: {classrooms}")
+
+        return BuildingClassroomsDTO(classrooms=classrooms)
+    except HTTPException:
+        raise
+    except Exception as e:
+        logger.error(f"(Get building classrooms) Error: {e}")
         raise HTTPException(status_code=500, detail="Internal server error")
--- backend/services/building_service.py
+++ backend/services/building_service.py
@@ -2,6 +2,7 @@ import logging
 
 from sqlalchemy.orm import Session
 
+from models.dto.classroom_dto import ClassroomDTO
 from models.tables.classroom import Classroom
 
 
@@ -20,4 +21,23 @@ class BuildingService:
             return sorted(unique_buildings)
         except Exception as e:
             self.logger.error(f"(Get all buildings) Error: {e}")
+            raise
+
+    async def get_all_classrooms_from_building(self, db: Session, building: int) -> list:
+        try:
+            classrooms = db.query(Classroom).filter(Classroom.building == building).all()
+
+            classrooms_dtos = [
+                ClassroomDTO(
+                    id=str(classroom.id),
+                    building=str(classroom.building),
+                    number=str(classroom.number),
+                    address=str(classroom.address)
+                ) for classroom in classrooms
+            ]
+
+            self.logger.info(f"(Get building classrooms) Success: {classrooms_dtos}")
+            return sorted(classrooms_dtos, key=lambda x: x.number)
+        except Exception as e:
+            self.logger.error(f"(Get building classrooms) Error: {e}")
             raise
