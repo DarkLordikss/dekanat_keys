292df2b7c84745641150cb70ec7d62181100818b
E1 <ivanboitsov386@gmail.com>
2024-02-28 04:50:19 +0700

    Добавлена рабочая навигационная панель
    - Реализован первичный вид навигационной панели
    - Созданы страницы-заглушки для страниц "Профиль", "Расписание", "Мои заявки" и "Ключи". На них пока ничего нет
    - Я поменял значок приложения. (Чё по кайфу то и делаю, сори)
    -------
    - Нужно убрать анимацию в навигационной панели или сделать её более адекватной
    - Сделать страницы-заглушки более адекватными

--- mobile/app/src/main/java/com/liid/dekanatkeys/activities/KeysActivity.kt
+++ mobile/app/src/main/java/com/liid/dekanatkeys/activities/KeysActivity.kt
@@ -1,26 +1,27 @@
 package com.liid.dekanatkeys.activities
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
 import com.liid.dekanatkeys.R
-import android.content.Intent
-import com.liid.dekanatkeys.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class KeysActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_keys)
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        bottomNavigationView.selectedItemId = R.id.bottom_account_circle
+        bottomNavigationView.selectedItemId = R.id.bottom_key
 
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.bottom_account_circle -> true
+                R.id.bottom_account_circle -> {
+                    startActivity(Intent(applicationContext, MainActivity::class.java))
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                    finish()
+                    true
+                }
                 R.id.bottom_calender_view -> {
                     startActivity(Intent(applicationContext, SheduleActivity::class.java))
                     overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
@@ -33,12 +34,8 @@ class MainActivity : AppCompatActivity() {
                     finish()
                     true
                 }
-                R.id.bottom_key -> {
-                    startActivity(Intent(applicationContext, KeysActivity::class.java))
-                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-                    finish()
-                    true
-                }
+                R.id.bottom_key -> true
+
                 else -> false
             }
         }
--- mobile/app/src/main/java/com/liid/dekanatkeys/activities/SheduleActivity.kt
+++ mobile/app/src/main/java/com/liid/dekanatkeys/activities/SheduleActivity.kt
@@ -1,32 +1,28 @@
 package com.liid.dekanatkeys.activities
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
 import com.liid.dekanatkeys.R
-import android.content.Intent
-import com.liid.dekanatkeys.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class SheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shedule)
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        bottomNavigationView.selectedItemId = R.id.bottom_account_circle
+        bottomNavigationView.selectedItemId = R.id.bottom_calender_view
 
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.bottom_account_circle -> true
-                R.id.bottom_calender_view -> {
-                    startActivity(Intent(applicationContext, SheduleActivity::class.java))
+                R.id.bottom_account_circle -> {
+                    startActivity(Intent(applicationContext, MainActivity::class.java))
                     overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
                     finish()
                     true
                 }
+                R.id.bottom_calender_view -> true
                 R.id.bottom_check_circle -> {
                     startActivity(Intent(applicationContext, MyApplicationsActivity::class.java))
                     overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
